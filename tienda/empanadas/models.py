3cdaff83ca96ebf0648da0f2c76bf648c89b3d48
Soan Gvoka <sgvoka@pci202-11.etud.iut-orsay.fr>
2024-09-03 10:02:21 +0200

    add Composition model

--- tienda/empanadas/models.py
+++ tienda/empanadas/models.py
@@ -16,5 +16,19 @@ class Empanada(models.Model) :
     prix = models.DecimalField(max_digits = 6, decimal_places = 2)
 
     def __str__(self) :
-        return 'empanada ' + self.nomEmpanada + ' (' + str(self.prix) + '€)'
+        return 'empanada ' + self.nomEmpanada + ' (prix: ' + str(self.prix) + '€)'
 
+
+class Composition(models.Model) :
+    class Meta :
+        unique_together = ('ingredient', 'empanada')
+    
+    idComposition = models.AutoField(primary_key = True)
+    ingredient = models.ForeignKey(Ingredient, on_delete = models.CASCADE)
+    empanada = models.ForeignKey(Empanada, on_delete = models.CASCADE)
+    quantite = models.CharField(max_length = 100)
+
+    def __str__(self) :
+        return self.ingredient.nomIngredient + "fait partie de l'empanada" \
+                + " '" + self.empanada.nomEmpanada + "' (qt: " + self.quantite + ")"
+    
