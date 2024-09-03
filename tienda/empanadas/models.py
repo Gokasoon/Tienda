21cc90c01adc634e8165f833268c134adbffbf05
Soan Gvoka <sgvoka@pci202-11.etud.iut-orsay.fr>
2024-09-03 09:49:10 +0200

    add Ingr and Empanada models + tables

--- tienda/empanadas/models.py
+++ tienda/empanadas/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model) :
+    idIngredient = models.AutoField(primary_key = True)
+    nomIngredient = models.CharField(max_length = 50)
+
+    def __str__(self) :
+        return self.nomIngredient
+
+
+class Empanada(models.Model) :
+    idEmpanada = models.AutoField(primary_key = True)
+    nomEmpanada = models.CharField(max_length = 50)
+    prix = models.DecimalField(max_digits = 6, decimal_places = 2)
+
+    def __str__(self) :
+        return 'empanada ' + self.nomEmpanada + ' (' + str(self.prix) + '€)'
+
