e3ba341215c89a99afb1bc1382e76d23f7a11240
Soan Gvoka <sgvoka@ETUD.IUT-ORSAY.FR>
2024-09-30 11:54:54 +0200

    connexion, login and logout working

--- tienda/comptes/urls.py
+++ tienda/comptes/urls.py
@@ -4,4 +4,6 @@ from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('login', LoginView.as_view(template_name="comptes/login.html"), name="login"),
+    path('logout', views.deconnexion, name="logout"),
+    path('connexion', views.connexion)
 ]
--- tienda/comptes/views.py
+++ tienda/comptes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def connexion(request) :
+    usr = request.POST['username']
+    pwd = request.POST['password']
+    user = authenticate(request, username=usr, password=pwd)
+    if user is None :
+        return redirect ('/login')
+    else :
+        login(request, user)
+        return redirect('/empanadas')
+
+def deconnexion(request) :
+    logout(request)
+    return render(request, 'comptes/logout.html')
