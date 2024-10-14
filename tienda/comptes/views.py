6eac49b1ea0d0b5f463612eb4f27c8119782e934
Soan Gvoka <sgvoka@ETUD.IUT-ORSAY.FR>
2024-10-14 10:24:09 +0200

    connexion protected

--- tienda/comptes/views.py
+++ tienda/comptes/views.py
@@ -3,13 +3,16 @@ from django.contrib.auth import authenticate, login, logout
 
 
 def connexion(request) :
-    usr = request.POST['username']
-    pwd = request.POST['password']
-    user = authenticate(request, username=usr, password=pwd)
-    if user is None :
-        return redirect ('/login')
+    if not request.user.is_authenticated :
+        usr = request.POST['username']
+        pwd = request.POST['password']
+        user = authenticate(request, username=usr, password=pwd)
+        if user is None :
+            return redirect ('/login')
+        else :
+            login(request, user)
+            return redirect('/empanadas')
     else :
-        login(request, user)
         return redirect('/empanadas')
 
 def deconnexion(request) :
