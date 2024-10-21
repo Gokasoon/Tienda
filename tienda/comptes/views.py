0e7af2d104da1ed6746162062c85be2d8637cebc
Soan Gvoka <sgvoka@pci202-11.etud.iut-orsay.fr>
2024-10-21 09:41:53 +0200

    register working

--- tienda/comptes/views.py
+++ tienda/comptes/views.py
@@ -43,3 +43,36 @@ def traitementFormulaireProfil(request) :
             return redirect('/empanadas')
     else :
         return redirect('/login')
+
+
+def formulaireInscription(request) :
+    user = None 
+    if not request.user.is_authenticated :
+        return render(
+            request,
+            'comptes/formulaireInscription.html'
+        )
+    else :
+        return render(
+            request,
+            'comptes/profil.html',
+            {
+                'user' : TiendaUser.objects.get(id=request.user.id),
+            }
+        )
+
+def traitementFormulaireInscription(request): 
+    user = TiendaUser() 
+    user.first_name = request.POST['first_name']
+    user.last_name = request.POST['last_name']
+    user.username = request.POST['username']
+    user.email = request.POST['email']
+    user.set_password(request.POST['password'])
+    img = request.FILES.get('image')
+    if img :
+        user.image = img
+    else:
+        user.image = "imagesUsers/default.png"
+    user.save() 
+    login(request, user) 
+    return redirect( '/empanadas' ) 
